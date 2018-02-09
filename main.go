0536f492f056e388a34bd3f0fa3bb2f1ccf64115
Заремба Александр <spbligr@gmail.com>
2018-02-09 18:56:21 +0300

    'sdfasdf'

--- main.go
+++ main.go
@@ -1,42 +1,5 @@
 package main
 
-import (
-	//"log"
-	//"fmt"
-	//"fmt"
-	//"fmt"
-	//"fmt"
-)
-
-
-
 func main() {
-
-	// println("run as\n\ngo test -v -race")
-	inputData := []int{0, 1, 1, 2, 3, 5, 8}
-	//testResult := "NOT_SET"
-
-	hashSignJobs := []job{
-		job(func(in, out chan interface{}) {
-			for _, fibNum := range inputData {
-				out <- fibNum
-			}
-		}),
-		job(SingleHash),
-		//job(MultiHash),
-		//job(CombineResults),
-		//job(func(in, out chan interface{}) {
-		//	dataRaw := <-in
-		//	data, ok := dataRaw.(string)
-		//	if !ok {
-		//		log.Fatal("cant convert result data to string")
-		//	}
-		//	testResult = data
-		//}),
-	}
-
-	ExecutePipeline(hashSignJobs...)
-
-	//fmt.Scanln()
+	println("run as\n\ngo test -v -race")
 }
-
--- signer.go
+++ signer.go
@@ -2,65 +2,149 @@ package main
 
 import (
 	"fmt"
-	//"sync"
-	//"log"
+	"log"
+	"sort"
+	"strconv"
+	"strings"
 	"sync"
 )
 
-func ExecutePipeline(freeFlowJobs... job)  {
+func ExecutePipeline(hashSignJobs ...job) {
+	wg := &sync.WaitGroup{}
+	defer wg.Wait()
 
 	in := make(chan interface{})
-	out := make(chan interface{})
-	wgIn := make(chan struct{})
 
+	for _, jobItem := range hashSignJobs {
+
+		wg.Add(1)
+
+		out := make(chan interface{})
+
+		go func(job job, in chan interface{}, out chan interface{}, wg *sync.WaitGroup) {
+			job(in, out)
+			defer wg.Done()
+			defer close(out)
+		}(jobItem, in, out, wg)
+
+		in = out
+	}
+
+}
+
+func SingleHash(in chan interface{}, out chan interface{}) {
 
 	wg := &sync.WaitGroup{}
 
-	for index, job := range freeFlowJobs {
+	tmp := make(chan string)
+
+	for data := range in {
+
+		fmt.Printf(" in %v", data.(int))
+
+
 		wg.Add(1)
-		go func(jobFunc func(in chan interface{}, out chan interface{}), in chan interface{}, out chan interface{}) {
-			go jobFunc(in, out)
-			select {
-			case <- in:
-				wgIn <- struct{}{}
-				if index == (len(freeFlowJobs) - 1) {
-					close(wgIn) //Кажется так не проканает. Попахивает говнокодом
-				}
+
+		raw, _ := data.(int)
+
+		data := strconv.Itoa(raw)
+		md5 := DataSignerMd5(data)
+
+		go func(wg *sync.WaitGroup, out chan string, d string, md5 string) {
+			hash := make(chan string)
+			hashMd5 := make(chan string) // make second because there is no guarante which will calculated first, so must take control of order.
+
+			go func(out chan string, input string) {
+				out <- DataSignerCrc32(input)
+			}(hash, d)
+
+			go func(out chan string, input string) {
+				out <- DataSignerCrc32(input)
+			}(hashMd5, md5)
+
+			out <- fmt.Sprintf("%v~%v", <-hash, <-hashMd5)
+			wg.Done()
+		}(wg, tmp, data, md5)
+	}
+
+	go func(wg *sync.WaitGroup, c chan string) {
+		defer close(c)
+		wg.Wait()
+	}(wg, tmp)
+
+	for hash := range tmp {
+		out <- hash
+	}
+}
+
+func MultiHash(in chan interface{}, out chan interface{}) {
+	type hashNode struct {
+		id    int
+		value string
+	}
+
+	wgOut := &sync.WaitGroup{}
+	outCh := make(chan string)
+
+	for input := range in {
+		wgIn := &sync.WaitGroup{}
+		data, ok := input.(string)
+		if !ok {
+			log.Fatalf("can't convert %T to string", input)
+		}
+		inCh := make(chan hashNode)
+
+		wgOut.Add(1)
+		wgIn.Add(6)
+		for i := 0; i < 6; i++ {
+			go func(wg *sync.WaitGroup, i int, inp string, out chan hashNode) {
+				defer wg.Done()
+				out <- hashNode{i, DataSignerCrc32(fmt.Sprintf("%v%v", i, inp))}
+			}(wgIn, i, data, inCh)
+		}
+		go func(wgInner *sync.WaitGroup, c chan hashNode) {
+			defer close(c)
+			wgInner.Wait()
+		}(wgIn, inCh)
+
+		go func(wg *sync.WaitGroup, in chan hashNode, out chan string) {
+			data := map[int]string{}
+			var dataKeys []int
+
+			for o := range in {
+				data[o.id] = o.value
+				dataKeys = append(dataKeys, o.id)
 			}
-		}(job, in, out)
+			sort.Ints(dataKeys)
 
+			var results []string
+			for i := range dataKeys {
+				results = append(results, data[i])
+			}
+
+			out <- strings.Join(results, "")
+			wg.Done()
+		}(wgOut, inCh, outCh)
 	}
 
-	for wgItem := range wgIn{
-		fmt.Println("gwItem", wgItem)
-		wg.Done() //тут только не очень понятно когда выйти из цикла
-	}
+	go func(wgOut *sync.WaitGroup, c chan string) {
+		defer close(c)
+		wgOut.Wait()
+	}(wgOut, outCh)
 
-	wg.Wait()
-
-}
-
-var SingleHash = func(in, out chan interface{})  {
-	for value := range out {
-
-		value = fmt.Sprintf("%s", value)
-
-		hashData := make(map[string]string)
-		result := DataSignerCrc32(hashData["data"]) + "~" + DataSignerCrc32(DataSignerMd5(hashData["data"]))
-
-		fmt.Println("SingleHash data= , resul = ", value, result)
-
-		in <- result
+	for hash := range outCh {
+		out <- hash
 	}
 }
 
-var MultiHash = func(in, out chan interface{}) {
-	for value := range in {
-		fmt.Println("MultiHash", value)
+func CombineResults(in, out chan interface{}) {
+	var result []string
+
+	for input := range in {
+		data, _ := input.(string)
+		result = append(result, data)
 	}
 
-}
-
-var CombineResults = func(in, out chan interface{}) {
-
+	sort.Strings(result)
+	out <- strings.Join(result, "_")
 }
